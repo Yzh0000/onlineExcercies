7f91bb350ffeee28ba1deeb47ddafe4c61dfa65e
xiaoqianglg8s <wangnanlg8s@gmail.com>
2017-01-11 17:13:56 +0800

    Qn76 cpp modified

--- 76-MinWindowSubstr/MinWindowSubstr.cpp
+++ 76-MinWindowSubstr/MinWindowSubstr.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> mp(128);
-        for(char c:t)   mp[c]++;
-        int begin=0,end=0,head=0,d=INT_MAX,count=t.size();
-        string res;
-        while(end<s.size()){
-            if(mp[s[end++]]-->0)    count--;
-            while(count==0){
-                if(end-begin<d)  d=end-(head=begin);
-                if(mp[s[begin++]]++==0)    count++;
-            }
-        }
-        return d==INT_MAX?"":s.substr(head,d);
+	    vector<int> mp(128);
+	    for (auto c : t) mp[c]++;
+
+	    int begin = 0, end = 0, count = t.size(), head = 0, d = INT_MAX;
+
+	    while (end < s.size()) {
+		    if (mp[s[end]] > 0) count--;
+		    mp[s[end]]--;
+		    end++;
+
+		    while (count == 0) {
+			    if (end - begin < d) {
+				    head = begin;
+				    d = end - begin;
+			    }
+			    mp[s[begin]]++;
+
+			    if (mp[s[begin]] > 0)   count++;
+			    begin++;
+		    }
+	    }
+	    return d==INT_MAX?"":s.substr(head,d);
     }
 };
